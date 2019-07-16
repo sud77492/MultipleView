cda88f65120836c12c7d9d5c94fc77353908ef28
sameer77492652 <Sameersharma078626@gmail.com>
2019-07-16 18:40:59 +0530

    contact us2

--- app/src/main/java/com/example/sudhanshus/multipleview/activity/ContactActivity.java
+++ app/src/main/java/com/example/sudhanshus/multipleview/activity/ContactActivity.java
@@ -4,15 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.example.sudhanshus.multipleview.R;
+import com.example.sudhanshus.multipleview.utils.Utils;
 
 public class ContactActivity extends AppCompatActivity {
 
-    AppCompatEditText aiName;
-    AppCompatEditText aiEmail;
-    AppCompatEditText aiPhone;
-    AppCompatEditText aiMessage;
+    AppCompatEditText acName;
+    AppCompatEditText acEmail;
+    AppCompatEditText acPhone;
+    AppCompatEditText acMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +26,18 @@ public class ContactActivity extends AppCompatActivity {
     }
 
     private void initListener() {
+        if(!validateData()) {
 
+        }else{
+            Utils.showLog(Log.ERROR, "VALIDATE", "FAIL", true);
+        }
     }
 
     private void initView() {
-
+        acName = findViewById(R.id.acName);
+        acEmail = findViewById(R.id.acEmail);
+        acPhone = findViewById(R.id.acPhone);
+        acMessage = findViewById(R.id.acMessage);
     }
 
     private void initToolbar() {
@@ -37,5 +46,32 @@ public class ContactActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(null);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //Tools.setSystemBarColor(this, R.color.amber_600);
+    }
+
+    private boolean validateData(){
+        Boolean check = false;
+        if(Utils.isEmpty(acName)){
+            acName.setError("Enter Name");
+            check = true;
+        }
+        if(!Utils.isValidEmail1(acEmail.getText().toString())){
+            acEmail.setError("Invalid Email");
+            check = true;
+        }
+        if(Utils.isEmpty(acEmail)){
+            acEmail.setError("Enter Email");
+            check = true;
+        }
+
+        if(Utils.isEmpty(acPhone)){
+            acPhone.setError("Enter Message");
+            check = true;
+        }
+        if(Utils.isEmpty(acMessage)){
+            acMessage.setError("Enter Message");
+            check = true;
+        }
+
+        return check;
     }
 }
